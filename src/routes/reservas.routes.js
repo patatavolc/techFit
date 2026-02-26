a460fbddefc3c84b15bb345c5f9252b881b10803
patatavolc <edermorosrodero@gmail.com>
2026-02-26 12:17:30 +0100

    servicio para reservar clase

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import { cancelarReserva } from '../controllers/reservas.controller.js';
+import express from "express";
+import { cancelarReserva } from "../controllers/reservas.controller.js";
 const router = express.Router();
 
-router.patch('/reservas/:id/cancelar', cancelarReserva);
-router.post('/reservas', reservarPlaza);
+router.patch("/reservas/:id/cancelar", cancelarReserva);
+router.post("/reservas", reservarPlaza);
 
 export default router;
--- src/services/reservas.service.js
+++ src/services/reservas.service.js
@@ -28,3 +28,58 @@ export async function cancelarReservaService(idReserva) {
   }
 }
 
+export async function reservarPlazaService(idUsuario, idClase) {
+  try {
+    pool.query("BEGIN");
+
+    const usuario = await pool.query(
+      `SELECT fecha_fin_suscripcion FROM usuarios WHERE id = $1`,
+      [idUsuario],
+    );
+
+    if (usuario.rows.length === 0) {
+      throw new Error("Usuario no encontrado");
+    }
+
+    const fechaSuscripcion = new Date(usuario.rows[0].fecha_fin_suscripcion);
+    const fechaActual = new Date();
+
+    if (fechaSuscripcion < fechaActual) {
+      throw new Error("La suscripción del usuario ha expirado");
+    }
+
+    const clase = await pool.query(
+      `SELECT aforo_maximo FROM clases WHERE id = $1`,
+      [idClase],
+    );
+
+    if (clase.rows.length === 0) {
+      throw new Error("Clase no encontrada");
+    }
+
+    const aforoMaximo = clase.rows[0].aforo_maximo;
+
+    const reservasExistentes = await pool.query(
+      `SELECT COUNT(*) FROM reservas WHERE id_clase = $1 AND estado_reserva = 'Confirmada'`,
+      [idClase],
+    );
+
+    const reservasActuales = parseInt(reservasExistentes.rows[0].count);
+
+    if (reservasActuales >= aforoMaximo) {
+      throw new Error("No hay plazas disponibles para esta clase");
+    }
+
+    const nuevaReserva = await pool.query(
+      `INSERT INTO reservas (id_usuario, id_clase, estado_reserva) VALUES ($1, $2, 'Confirmada') RETURNING *`,
+      [idUsuario, idClase],
+    );
+
+    await pool.query("COMMIT");
+
+    return nuevaReserva.rows[0];
+  } catch (error) {
+    await pool.query("ROLLBACK");
+    throw new Error(`Error al reservar la plaza: ${error.message}`);
+  }
+}
